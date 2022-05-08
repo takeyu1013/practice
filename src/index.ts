1fa5bc047d86e53f7f516bd93058c8a5d882f7e2
takeyu1013 <yuto1013@gmail.com>
2022-05-08 18:36:45 +0900

    ✨

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 const message: string = "Hello, world!";
 
 console.log(message);
+
+type User = {
+  id: number;
+  name: string;
+};
+
+const func = (user: Readonly<User>) => {
+  const { id, name } = user;
+  console.log(id, name);
+};
+
+const user: User = {
+  id: 1,
+  name: "foo",
+};
+
+func(user);
