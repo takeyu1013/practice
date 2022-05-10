d3fa152b8bd61280c902185ad883d8b610fe84d7
takeyu1013 <yuto1013@gmail.com>
2022-05-10 22:06:51 +0900

    ✨

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,45 @@ const message: string = "Hello, world!";
 console.log(message);
 
 type User = Readonly<{
-  id: number;
   name: string;
+  age: number;
+  premiumUser: boolean;
 }>;
 
-const func = ({ id, name }: User) => {
-  console.log(id, name);
+const data: string = `
+uhyo,26,1
+John Smith,17,0
+Mary Sue,14,1
+`;
+
+const func = ({ name, age }: User) => {
+  console.log(name, age);
 };
 
 const user: User = {
-  id: 1,
   name: "foo",
+  age: 20,
+  premiumUser: true,
 };
 
 func(user);
+
+const users = data
+  .split("\n")
+  .filter((line) => line)
+  .map((line): User => {
+    const [name, ageString, premiumUserString] = line.split(",");
+    const age = Number(ageString);
+    const premiumUser = premiumUserString === "1";
+    return { name, age, premiumUser };
+  });
+
+users.forEach((user) => {
+  if (user.premiumUser) {
+    console.log(`${user.name} (${user.age}) はプレミアムユーザーです。`);
+  } else {
+    console.log(
+      `${user.name} (${user.age}) はプレミアムユーザーではありません。`
+    );
+  }
+});
