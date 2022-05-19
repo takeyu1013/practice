d664e56a0d6f76bb30cf4a7205bc5e7f8b8b6d17
takeyu1013 <yuto1013@gmail.com>
2022-05-19 21:36:29 +0900

    ✨

--- src/section5.ts
+++ src/section5.ts
@@ -1,21 +1,27 @@
 export const section5 = () => {
   class User {
-    #name: string;
-    #age: number;
+    readonly #name: string;
+    readonly #age: number;
+
     constructor(name: string, age: number) {
-      this.#name = name;
-      this.#age = age;
-    }
-    static createUser(name: string, age: number) {
       if (name === "") {
         throw new Error("名前は空にできません！");
       }
-      return new User(name, age);
+      this.#name = name;
+      this.#age = age;
     }
+
     getMessage(message: string) {
       return `${this.#name} (${this.#age} 「${message}」)`;
     }
   }
-  const uhyo = User.createUser("uhyo", 26);
+  const createUser = (name: string, age: number) => {
+    return (message: string) => {
+      return `${name} (${age} 「${message}」)`;
+    };
+  };
+  const uhyo = new User("uhyo", 26);
   console.log(uhyo.getMessage("こんにちは"));
+  const getMessage = createUser("uhyo", 26);
+  console.log(getMessage("こんにちは"));
 };
