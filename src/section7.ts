1e37ee91e493cbe30599ced1f16e3823f1788511
takeyu1013 <yuto1013@gmail.com>
2022-06-11 15:16:22 +0900

    ♻️

--- src/section7.ts
+++ src/section7.ts
@@ -2,16 +2,6 @@ import { readFileSync } from "fs";
 
 export const section7 = () => {
   const data = readFileSync("uhyo.txt", "utf-8");
-  let count = 0;
-  let currentIndex = 0;
-  while (true) {
-    const nextIndex = data.indexOf("uhyo", currentIndex);
-    if (nextIndex >= 0) {
-      ++count;
-      currentIndex = nextIndex + 1;
-    } else {
-      break;
-    }
-  }
+  const count = data.split("uhyo").length - 1;
   console.log(count);
 };
